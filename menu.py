90b1c44841ec602b01469492552e2004334cc2b9
Семиглазов Алексей Викторович <a.semiglazov@tes.local>
2023-02-13 08:53:38 +0300

    1

--- menu.py
+++ menu.py
@@ -1,28 +1,40 @@
 ﻿# -*- coding: utf-8 -*-
-from PyQt5.QtWidgets import QAction
-from PyQt5 import QtSql,QtCore,QtWidgets
+from PyQt5 import QtWidgets
 class Menu():
         #Создание меню главного окна
     def createMenu(self):
-        fileMenuTrigered=QAction("Инфо",self)
-        fileMenuTrigered.triggered.connect(self.showfileMenu)
         menubar=self.menuBar()
-        fileMenu=menubar.addMenu("Файл")
-        fileMenu.addAction(fileMenuTrigered)
+        subMenu=menubar.addMenu("Отчеты")
+        #Создание подменю "Отчет о питании"
+        eatMenuTrigered=QtWidgets.QAction("Отчет о питании",self)
+        eatMenuTrigered.triggered.connect(self.eatMenuClicked)
+        subMenu.addAction(eatMenuTrigered)
+        #Создание подменю "Рабочий отчет"
+        workMenuTrigered=QtWidgets.QAction("Рабочий отчет",self)
+        workMenuTrigered.triggered.connect(self.workMenuClicked)
+        subMenu.addAction(workMenuTrigered)
 
-        #Обработчик события нажатия на "Инфо"
-    def showfileMenu(self):
-        self.datawindow=QtWidgets.QTableView(self)
-        con = QtSql.QSqlDatabase.addDatabase('QSQLITE')
-        con.setDatabaseName("db\\testdb.db3")
-        con.open()
-        sqm = QtSql.QSqlQueryModel(self)
-        sqm.setQuery('select * from test')
-        sqm.setHeaderData(1, QtCore.Qt.Horizontal, 'Имя')
-        sqm.setHeaderData(2, QtCore.Qt.Horizontal, 'Игровой номер')
-        self.datawindow.setModel(sqm)
-        self.datawindow.hideColumn(0)
-        self.datawindow.setColumnWidth(1, 150)
-        self.datawindow.setColumnWidth(2,150)
-        self.datawindow.resize(600,400)
-        self.datawindow.show()
+        #Обработчик события нажатия на "Отчет о питании"
+    def eatMenuClicked(self):
+        self.eatCalendar=QtWidgets.QCalendarWidget(parent=self)
+        self.eatCalendar.resize(200,200)
+        self.eatCalendar.show()
+        self.eatCalendar.activated.connect(self.showTwoDate)    
+        #Обработчик события двойного нажатия левой кнопкой мыши на дату QCalendarWidget
+    def showTwoDate(self):
+        #print(self.eatCalendar.selectedDate())
+        print("showTwoDate")
+    
+        
+
+
+
+
+
+        #Обработчик события нажатия на "Рабочий отчет"
+    def workMenuClicked(self):
+        self.window=QtWidgets.QWidget()
+        self.window.resize(100,100)
+        self.window.show()
+
+        
