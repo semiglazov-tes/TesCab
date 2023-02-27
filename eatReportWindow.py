8523dda6ecea1e46cac2893565c1ab82406fc808
semiglazov-tes <semiglazov_tes@mail.ru>
2023-02-27 22:59:45 +0300

    Update eatReportWindow.py
    Добавление записи в БД при двойном нажатии ПКМ на дате календаря

--- eatReportWindow.py
+++ eatReportWindow.py
@@ -1,4 +1,5 @@
-﻿from PyQt5 import QtWidgets,QtCore,QtSql      
+﻿from PyQt5 import QtWidgets,QtCore,QtSql
+      
 class EatReportWindow(QtWidgets.QWidget):
         #Создание окна отчетов по питанию
     def __init__(self,mainWindow):
@@ -7,41 +8,32 @@ class EatReportWindow(QtWidgets.QWidget):
         self.container=QtWidgets.QWidget()
         self.container.setParent(mainWindow)
         #Создание календаря
-        self.eatCalendar=QtWidgets.QCalendarWidget()
+        self.eatCalendar=EatCalendar()
         #Создание таблицы дата/сумма
         self.eatTable=EatTabletView()
         self.eatTable.initializationEatTableView()
-        self.eatTable
         #Создание контейнера для выравнивания виджетов по горизонтали
         self.hbox =QtWidgets.QHBoxLayout()
         self.hbox.addWidget(self.eatTable)
         self.hbox.addWidget(self.eatCalendar)
         #добавление в основной контейнер 
         self.container.setLayout(self.hbox)
-        
-        #расположение окна отчетов по питанию по центру главного окна
+        #расположение окна отчетов по питанию по центру главного окна и его отображение
         x=(mainWindow.width()-self.container.width())//2
         y=(mainWindow.height()-self.container.height())//2
         self.container.move(x,y)
         self.container.show()
 
-        #привязка события двойного щелчка ЛКМ на дату
-        self.eatCalendar.activated.connect(self.twoClickOnDate) 
-
-        #Обработчик события двойного нажатия левой кнопкой мыши на дату QCalendarWidget
-    def twoClickOnDate(self):
-        selectedDateStr=self.eatCalendar.selectedDate().toString("yyyy.MM.dd")
-        print(selectedDateStr)
-
 class EatTabletView(QtWidgets.QTableView):
     def __init__(self):
         super().__init__()
-        self.sizePolicy=QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Expanding);
+        self.sizePolicy=QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Expanding)
         self.setSizePolicy(self.sizePolicy)
         self.verticalHeader().setVisible(False)
         self.horizontalHeader().setSectionResizeMode(1)
     """def resizeEvent(self, event):   self.resize(event.size())"""
-
+    
+    #Первичное отображение таблицы дата-сумма при превом открытии вкладки "Отчет о питании"
     def initializationEatTableView(self):
         #Создание подключения к базе данных для первичного отображения таблицы-оточета по питанию
         con = QtSql.QSqlDatabase.addDatabase('QSQLITE')
@@ -53,7 +45,23 @@ class EatTabletView(QtWidgets.QTableView):
         sqm.setHeaderData(2, QtCore.Qt.Horizontal, 'Сумма')
         self.setModel(sqm)
         self.hideColumn(0)
-        self.show()
+        if sqm.rowCount()==0:
+            self.hide()
         con.close()
-    
+        
+class EatCalendar(QtWidgets.QCalendarWidget):
+    def __init__(self):
+        super().__init__()
+        #привязка события двойного щелчка ЛКМ на дату
+        self.activated.connect(self.twoClickOnDate)
+         
+        #Обработчик события двойного нажатия левой кнопкой мыши на дату QCalendarWidget
+    def twoClickOnDate(self):
+        selectedDateStr=self.selectedDate().toString("yyyy.MM.dd")
+        con = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+        con.setDatabaseName("db\\tesCabDb.db3")
+        con.open()
+        sqm = QtSql.QSqlQueryModel()
+        sqm.setQuery('insert into tesEatReport values () ')
+        print(selectedDateStr)
         
